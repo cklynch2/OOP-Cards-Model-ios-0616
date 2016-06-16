874b4d18fb4bae8b57a6971389de4236abde1939
cklynch2 <cklynch2@gmail.com>
2016-06-16 10:38:51 -0400

    Model for playing cards and shuffling a deck

--- OOP-Cards-Model/FISAppDelegate.m
+++ OOP-Cards-Model/FISAppDelegate.m
@@ -1,11 +1,29 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISCard.h"
+#import "FISCardDeck.h"
 
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    FISCard *test = [[FISCard alloc] init];
+    NSLog(@"%@", test.description);
+    
+    FISCardDeck *newDeck = [[FISCardDeck alloc] init];
+    NSLog(@"%@", newDeck.description);
+    
+    [newDeck shuffleRemainingCards];
+    NSLog(@"This is the shuffled deck: %@", newDeck.description);
+    
+    FISCard *cardOneRoundOne = [newDeck drawNextCard];
+    FISCard *cardTwoRoundOne = [newDeck drawNextCard];
+    NSLog(@"Which card won the first round? Card one: %@, Card two: %@", cardOneRoundOne.description, cardTwoRoundOne.description);
+    NSLog(@"What cards remain in the deck? %@", newDeck.description);
+    NSLog(@"What cards have been dealt? %@", newDeck.dealtCards);
+    
     return YES;
 }
 
